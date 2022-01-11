f4a6d3c95c304240d9e7e8c3bdf632bb57a16f30
Nakamol180 <nakamol_nuang@cmu.ac.th>
2022-01-11 21:16:00 +0700

    "complete"

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 const int N = 5;
 
-void inputMatrix(double [][N]);
+void inputMatrix(double s[][N]);
 
-void findLocalMax(const double [][N], bool [][N]);
+void findLocalMax(const double s[][N], bool r[][N]);
 
-void showMatrix(const bool [][N]);
+void showMatrix(const bool r[][N]);
 
 int main(){
 	double A[N][N]; 
@@ -18,4 +18,42 @@ int main(){
 	cout << "Result is \n";
 	showMatrix(B);
 	return 0;
+}
+
+void inputMatrix(double s[][N]){
+	for(int i = 0; i < N; i++){
+		cout << "Row" << i + 1 << ": ";
+		for(int j = 0; j < N; j++){
+			cin >> s[i][j];
+		}
+	}
+}
+
+void showMatrix(const bool r[][N]){
+	int i,j;
+	for( i = 0; i < N; i++){
+		for( j = 0; j < N; j++){
+			if(r[i][j] == true)
+				cout << "1 ";
+			else cout << "0 ";
+		}
+		cout << "\n";
+	}
+}
+
+void findLocalMax(const double s[][N], bool r[][N]){
+	int x,y;
+	for( x = 0; x < N; x++){
+		for( y = 0; y < N; y++){
+			if(s[x][y] < s[x][y+1] || s[x][y] < s[x][y-1] || s[x][y] < s[x+1][y] || s[x][y]<s[x-1][y]){
+				r[x][y] = 0;
+			}else{r[x][y] = 1;
+			r[0][y] = 0;
+			r[N-1][y] = 0;
+			}
+		}
+		r[x][0] = 0;
+		r[x][N-1] = 0;
+
+	}
 }
